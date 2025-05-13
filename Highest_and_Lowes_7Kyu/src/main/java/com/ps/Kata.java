cdf1a6a5e3b9d0f236df8295d9eee07b7e3a32e0
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-05-13 00:06:42 -0400

    Solved

--- Highest_and_Lowes_7Kyu/src/main/java/com/ps/Kata.java
+++ Highest_and_Lowes_7Kyu/src/main/java/com/ps/Kata.java
@@ -1,4 +1,24 @@
 package com.ps;
 
 public class Kata {
+    public static String highAndLow(String numbers) {
+        //
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        String[] numbersParsed = numbers.split(" ");
+        for (int i = 0; i < numbersParsed.length; i++) {
+
+
+            int convertedNumber = Integer.parseInt(numbersParsed[i]);
+            if (convertedNumber < min){
+                min = convertedNumber;
+            } else if (convertedNumber > max) {
+                max = convertedNumber;
+            }
+
+        }
+        return max + " " + min;
+    }
+
 }
--- Highest_and_Lowes_7Kyu/src/main/java/com/ps/Main.java
+++ Highest_and_Lowes_7Kyu/src/main/java/com/ps/Main.java
@@ -2,6 +2,8 @@ package com.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String result = Kata.highAndLow("1 2 3 4 5");
+        System.out.println(result);
     }
 }
