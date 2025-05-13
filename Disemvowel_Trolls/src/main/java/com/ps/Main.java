2246c4bb55289a7bf53e49afa4969b1ddb604df4
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-05-13 08:48:12 -0400

    Disemvowel Trolls

---
+++ Disemvowel_Trolls/src/main/java/com/ps/Main.java
@@ -0,0 +1,9 @@
+package com.ps;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String result = Kata.highAndLow("609 2 3 4 -925");
+//        System.out.println(result);
+    }
+}
