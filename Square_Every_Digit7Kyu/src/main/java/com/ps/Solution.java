258037ad7cf070e3fa638062599e75ad01982797
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-05-16 20:27:45 -0400

    Completed

--- Square_Every_Digit7Kyu/src/main/java/com/ps/Solution.java
+++ Square_Every_Digit7Kyu/src/main/java/com/ps/Solution.java
@@ -1,4 +1,21 @@
 package com.ps;
 
 public class Solution {
+
+    public int squareDigits(int n) {
+
+        StringBuilder answer = new StringBuilder();
+        String convertedNum = String.valueOf(n);
+
+        for (int i = 0; i <convertedNum.length(); i++) {
+        int currentDigit = Character.getNumericValue(convertedNum.charAt(i));
+        answer.append(currentDigit*currentDigit);
+        }
+
+
+
+            return Integer.parseInt(answer.toString());
+    }
+
+
 }
